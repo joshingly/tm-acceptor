610ba907cd2996ed303f2c2be1df82ce30447688
antonishen <joshua@antonishen.com>
2011-10-09 20:20:18 -0400

    Initial commit

---
+++ lib/turing_machine.rb
@@ -0,0 +1,56 @@
+# Joshua Antonishen
+# 10/2/11
+
+class TuringMachine
+  attr_reader :transition_table
+
+  def initialize( transition_table_file )
+    @transition_table = load_transition_table( transition_table_file )
+  end
+
+  def load_transition_table( transition_table_file )
+    tm = Hash.new( false )
+
+    File.open( transition_table_file, "r" ) do | file |
+      all_lines = file.read
+      all_lines.split(/\n/).each do | line |
+        state_name, final, *transitions = line.split(/,/).map { | item | item.to_sym }
+        final = ( final == :true ) ? true : false
+
+        tm[state_name] = Hash.new( false )
+        tm[state_name][:final] = final
+
+        transitions.each_slice(4) do | transition |
+          tm[state_name][transition[1]] = { new_state: transition[0],
+                                            output: transition[2],
+                                            direction: transition[3] }
+        end
+      end
+    end
+
+    @transition_table = tm
+  end
+
+  def simulate( string )
+    current_state = :q0
+    position = 0
+
+    while( true )
+      break if position == string.length
+      transition = @transition_table[current_state][string[position].to_sym]
+      break unless transition
+
+      move = 1 if transition[:direction] == :R
+      move = -1 if transition[:direction] == :L
+
+      string[position] = transition[:output].to_s
+      current_state = transition[:new_state]
+      position += move
+
+      return true if position == string.length && @transition_table[current_state][:final]
+    end
+
+    return false
+  end
+
+end
---
+++ readme.md
@@ -0,0 +1,17 @@
+Turing Machine String Acceptor
+==
+
+Given a transition table and a string, determine whether the turing machine described by the given transition table will accept or reject the given string.
+
+Transition Table Specification
+--
+
+list of value separated by commas each line MUST have at least two items, the state name and true or false indicating whether the state is a final state. After that in group of 4 you have new state name, input, output, direction to move tape. look at ex1, ex2, and ex3 for examples.
+
+Usage
+--
+See program.rb for examples...
+
+Tested On...
+--
+Tested on Ruby-1.9.2-p290
