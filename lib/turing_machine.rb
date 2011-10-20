6e2530bc8e474155a643ad4aa699745153d828fc
antonishen <joshua@antonishen.com>
2011-10-20 13:46:34 -0400

    Style changes

--- lib/turing_machine.rb
+++ lib/turing_machine.rb
@@ -4,23 +4,23 @@
 class TuringMachine
   attr_reader :transition_table
 
-  def initialize( transition_table_file )
-    @transition_table = load_transition_table( transition_table_file )
+  def initialize(transition_table_file)
+    @transition_table = load_transition_table(transition_table_file)
   end
 
-  def load_transition_table( transition_table_file )
-    tm = Hash.new( false )
+  def load_transition_table(transition_table_file)
+    tm = Hash.new(false)
 
-    File.open( transition_table_file, "r" ) do | file |
+    File.open(transition_table_file, "r") do |file|
       all_lines = file.read
-      all_lines.split(/\n/).each do | line |
-        state_name, final, *transitions = line.split(/,/).map { | item | item.to_sym }
-        final = ( final == :true ) ? true : false
+      all_lines.split(/\n/).each do |line|
+        state_name, final, *transitions = line.split(/,/).map { |item| item.to_sym }
+        final = (final == :true) ? true : false
 
-        tm[state_name] = Hash.new( false )
+        tm[state_name] = Hash.new(false)
         tm[state_name][:final] = final
 
-        transitions.each_slice(4) do | transition |
+        transitions.each_slice(4) do |transition|
           tm[state_name][transition[1]] = { new_state: transition[0],
                                             output: transition[2],
                                             direction: transition[3] }
@@ -31,19 +31,19 @@ class TuringMachine
     tm
   end
 
-  def simulate( tape )
+  def simulate(tape)
     current_state = :q0
     position = 0
 
-    while( true )
+    while(true)
       # infinite tape to left
-      if ( position == -1 )
+      if (position == -1)
         position = 0
         tape = "/" << tape
       end
 
       # infinte tape to right
-      tape[position] = "/" if ( position == tape.length )
+      tape[position] = "/" if (position == tape.length)
 
       transition = @transition_table[current_state][tape[position].to_sym]
       break unless transition
--- program.rb
+++ program.rb
@@ -1,23 +1,23 @@
 #!/usr/bin/env ruby
 
-$: << "#{ File.dirname( __FILE__ ) }/lib/"
+$: << "#{ File.dirname(__FILE__) }/lib/"
 require 'turing_machine'
 
 puts "---------------------------------------------"
 puts "ex1 accepts the language a^i b^i c^i, i >= 0"
-tm1 = TuringMachine.new( "files/ex1.txt" )
-puts "BaabbccB: #{ tm1.simulate( "BaabbccB" ) }"
-puts "BaabbccB: #{ tm1.simulate( "BaabbbccB" ) }"
+tm1 = TuringMachine.new("files/ex1.txt")
+puts "BaabbccB: #{ tm1.simulate("BaabbccB") }"
+puts "BaabbccB: #{ tm1.simulate("BaabbbccB") }"
 
 puts "---------------------------------------------"
 puts "ex2 accepts the language (a | b)*"
-tm2 = TuringMachine.new( "files/ex2.txt" )
-puts "BabababababaaabababababababB: #{ tm2.simulate( "BabababababaaabababababababB" ) }"
-puts "BabababababaaabcababababababB: #{ tm2.simulate( "BabababababaaabcababababababB" ) }"
+tm2 = TuringMachine.new("files/ex2.txt")
+puts "BabababababaaabababababababB: #{ tm2.simulate("BabababababaaabababababababB") }"
+puts "BabababababaaabcababababababB: #{ tm2.simulate("BabababababaaabcababababababB") }"
 
 puts "---------------------------------------------"
 puts "ex3 accepts the language aa (a | b)*"
-tm3 = TuringMachine.new( "files/ex3.txt" )
-puts "BaabbababababbbbB: #{ tm3.simulate( "BaabbababababbbbB" ) }"
-puts "BabbbbabaB: #{ tm3.simulate( "BabbbbabaB" ) }"
+tm3 = TuringMachine.new("files/ex3.txt")
+puts "BaabbababababbbbB: #{ tm3.simulate("BaabbababababbbbB") }"
+puts "BabbbbabaB: #{ tm3.simulate("BabbbbabaB") }"
 
